c1e6e60973807690cae3a26b757779216a336ebc
MangeshAwate <mawate9@gmail.com>
2022-11-30 18:05:08 +0530

    login completed

--- fllogin.js
+++ fllogin.js
@@ -1,2 +1,12 @@
 let reg_email="andrewsiamond80@gmail.com";
 let reg_pass="paasword";
+
+let enter_email="andrewsiamond90@gmail.com";
+let enter_pass="password";
+
+if(reg_email==enter_emai){
+console.log("login successful")
+}
+else{
+console.log("login failed")
+}
